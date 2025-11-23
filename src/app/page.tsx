6ae9bab426f6866218269a5233f073fbbfef4d9e
Faruq <umarulfaruqq123@gmail.com>
2025-11-23 22:33:18 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    // Setiap produk kerajinan punya makna, dan setiap pembeli bisa membuat cerita (story) mereka sendiri dari barang itu.
+    <div className='relative aspect-3/1 mb-12 rounded-lg overflow-hidden'>
+      <Image src="/a.jpg" alt="Bamboo" fill className="object-cover" />
+    </div>
   )
 }
 
